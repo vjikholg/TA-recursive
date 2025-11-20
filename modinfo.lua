4055425b03b88c28a28cb0fcd0c7cd3b85cd60a4
vjikholg <yuvict1@outlook.com>
2025-11-19 23:20:50 -0800

    fixed modinfo (for real this time)

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='test-5c0c28e',
+  version='recursive-11634-79c7172',
   mutator='',
   game='',
   shortGame='TA',
